6c8e6c01c2fd65d5dfbf4a45ec4381e416a038bd
nyoken <dk525p.ss16@gmail.com>
2020-08-21 02:30:42 +0900

    Stationテーブルのtypeカラムをbroadcast_typeに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_100456) do
+ActiveRecord::Schema.define(version: 2020_08_20_165409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2020_08_16_100456) do
 
   create_table "stations", force: :cascade do |t|
     t.string "name", null: false
-    t.string "type", null: false
+    t.string "broadcast_type", null: false
     t.bigint "program_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
