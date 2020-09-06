22993546480be9dd8e58ccac8b6e0822669e38c0
nyoken <dk525p.ss16@gmail.com>
2020-09-07 02:40:45 +0900

    放送局名による絞り込みを実装

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,11 @@
 class ProgramsController < ApplicationController
   def index
     @programs = Program.all.includes(:personalities, :station, corners: :songs).order(:day)
+    @stations = Station.select(:name).distinct
+
+    if params[:sort]
+      program_ids = Station.where(name: params[:sort]).pluck(:program_id)
+      @programs = @programs.where(id: program_ids)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'programs#index'
+  get ':sort', to: 'programs#index', as: 'sort'
 end
